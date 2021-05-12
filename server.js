c6dd1abef7ab0a4869620ec93002a9f2c0d8fa56
Adnan Reza <adnan.reza@outlook.com>
2021-05-12 10:14:33 -0700

    Setup Skeleton GraphQL server with hardcoded data and root query

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema.js')
+
+const app = express();
+
+/**
+ * Entry point for GraphQL
+ */
+app.use('/graphql', graphqlHTTP({
+    schema:schema,
+    graphiql:true
+}))
+
+app.listen('4000', () => {
+    console.log('Server is running on port 4000...')
+})
+
